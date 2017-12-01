1ad833978d5cceb0054fdb6b2dea46366eb5d74b
deston-trace <destontrace@gmail.com>
2017-12-01 19:06:52 +0500

    added firstname and last name to user model and update signup and edit user data views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   get 'welcome/index'
   get 'welcome/about'
 
